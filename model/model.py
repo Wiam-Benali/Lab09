bac8915775b3572c21e7ded386494374ba83070c
Wiam-Benali <wiambenali2005@gmail.com>
2025-11-23 12:14:27 +0100

    seconda_modifica

--- model/model.py
+++ model/model.py
@@ -13,9 +13,6 @@ class Model:
         self._valore_ottimo: int = -1
         self._costo = 0
 
-        self._tour_per_attrazione = {}
-        self._attrazione_per_tour = {}
-
         # Caricamento
         self.load_tour()
         self.load_attrazioni()
@@ -43,18 +40,10 @@ class Model:
         """
         relazioni = TourDAO.get_tour_attrazioni()
         for relazione in relazioni:
-            if relazione['id_tour'] not in self._attrazione_per_tour:
-                self._attrazione_per_tour[relazione['id_tour']] = set()
-                self._attrazione_per_tour[relazione['id_tour']].add(relazione['id_attrazione'])
-            else:
-                self._attrazione_per_tour[relazione['id_tour']].add(relazione['id_attrazione'])
-            if relazione['id_attrazione'] not in self._tour_per_attrazione:
-                self._tour_per_attrazione[relazione['id_attrazione']] = set()
-                self._tour_per_attrazione[relazione['id_attrazione']].add(relazione['id_tour'])
-            else:
-                self._tour_per_attrazione[relazione['id_attrazione']].add(relazione['id_tour'])
-
-
+             # creo una tupla (nome attrazione, valore culturale) in modo da tenere traccia del valore culturale dato che non ho più l'id dell'attrazione
+            self.tour_map[relazione['id_tour']].attrazioni.add((self.attrazioni_map[relazione['id_attrazione']].nome,
+                                                               self.attrazioni_map[relazione['id_attrazione']].valore_culturale))
+            self.attrazioni_map[relazione['id_attrazione']].tour.add(relazione['id_tour'])
 
 
     def genera_pacchetto(self, id_regione: str, max_giorni: int = None, max_budget: float = None):
@@ -79,7 +68,7 @@ class Model:
     def _ricorsione(self, start_index: int, pacchetto_parziale: list, durata_corrente: int, costo_corrente: float, valore_corrente: int, attrazioni_usate: set,max_giorni: int, max_budget: float,tour_regione):
         """ Algoritmo di ricorsione che deve trovare il pacchetto che massimizza il valore culturale"""
 
-        if self._valore_ottimo == -1 or valore_corrente > self._valore_ottimo:
+        if self._valore_ottimo == -1 or (valore_corrente > self._valore_ottimo):
             self._costo = costo_corrente
             self._pacchetto_ottimo = copy.deepcopy(pacchetto_parziale)
             self._valore_ottimo = valore_corrente
@@ -91,15 +80,17 @@ class Model:
             if self.soluzione_valida(tour,durata_corrente,costo_corrente,attrazioni_usate,max_giorni,max_budget):
 
                 pacchetto_parziale.append(tour)
-                costo_corrente += float(tour.costo)
+                nuovo_costo_corrente = costo_corrente + float(tour.costo)
+                nuovo_valore_corrente = valore_corrente
+                nuove_attrazioni_usate = attrazioni_usate
+                for attrazione in self.tour_map[tour.id].attrazioni:
+                    nuovo_valore_corrente += attrazione[1]
+                    nuove_attrazioni_usate.add(attrazione[0])
+                nuova_durata_corrente = durata_corrente + tour.durata_giorni
 
-                for attrazione in self._attrazione_per_tour[tour.id]:
-                    valore_corrente += self.attrazioni_map[attrazione].valore_culturale
-                    attrazioni_usate.add(self.attrazioni_map[attrazione].nome)
-                durata_corrente += tour.durata_giorni
-
-                self._ricorsione((i+1),pacchetto_parziale,durata_corrente,costo_corrente,valore_corrente,attrazioni_usate,max_giorni,max_budget,tour_regione)
+                self._ricorsione((i+1),pacchetto_parziale,nuova_durata_corrente,nuovo_costo_corrente,nuovo_valore_corrente,nuove_attrazioni_usate,max_giorni,max_budget,tour_regione)
                 pacchetto_parziale.pop()
+
 
     def soluzione_valida(self, tour,durata_corrente,costo_corrente,attrazioni_usate,max_giorni,max_budget):
         if durata_corrente + tour.durata_giorni > max_giorni:
@@ -108,8 +99,8 @@ class Model:
             return False
 
         attrazioni = set()
-        for attrazione in self._attrazione_per_tour[tour.id]:
-            attrazioni.add(self.attrazioni_map[attrazione].nome)
+        for attrazione in self.tour_map[tour.id].attrazioni:
+            attrazioni.add(attrazione)
         if len(attrazioni_usate.intersection(attrazioni))>=1:
             return False
         return True
